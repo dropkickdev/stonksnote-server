6065e9e2d7a2581ff2191705529fe4cde9e98caf
enchance <enchance@gmail.com>
2021-07-25 22:31:04 +0800

    Completed tortoise queries

--- app/tests/routes.py
+++ app/tests/routes.py
@@ -33,13 +33,7 @@ def readcookie(refresh_token: Optional[str] = Cookie(None)):
 @testrouter.get('/tortoise')
 async def dev_user_data(_: Response, user=Depends(current_user)):
     
-    # # 1: Same Table
-    # a = await Equity.get(ticker='COL').only('id', 'ticker', 'status')                   # 1q
-    # ic(type(a), a, vars(a))
-    
-    # -------------------------------------------------------------------------------------------
-    
-    # 2.1: FK Table
+    # FK Relationships
     query = Equity.get(ticker='COL')
     
     """
@@ -50,9 +44,9 @@ async def dev_user_data(_: Response, user=Depends(current_user)):
     - owner__author__display and name contain field data not an object
     TOTAL QUERIES: 1
     """
-    torun = query.values('id', 'ticker', 'owner__author__display', name='owner__name')  # partial fields
-    pat1 = await torun
-    ic(type(pat1), pat1)
+    # torun = query.values('id', 'ticker', 'owner__author__display', name='owner__name')  # partial fields
+    # pat1 = await torun
+    # ic(type(pat1), pat1)
     
     """
     PATTERN 2a: ForeignKeyField
@@ -62,10 +56,10 @@ async def dev_user_data(_: Response, user=Depends(current_user)):
     - The owner_id field must exist
     TOTAL QUERIES: 2
     """
-    torun = query.only('id', 'owner_id')    # partial fields
-    pat2a = await torun
-    the_owner = await pat2a.owner.only('id', 'name')   # +1q, partial fields
-    ic(type(pat2a), vars(pat2a), type(the_owner), vars(the_owner))
+    # torun = query.only('id', 'owner_id')    # partial fields
+    # pat2a = await torun
+    # the_owner = await pat2a.owner.only('id', 'name')   # +1q, partial fields
+    # ic(type(pat2a), vars(pat2a), type(the_owner), vars(the_owner))
     
     """
     PATTERN 2b: ForeignKeyField
@@ -76,11 +70,11 @@ async def dev_user_data(_: Response, user=Depends(current_user)):
     - Better to use prefetch_related instead
     TOTAL QUERIES: 2
     """
-    torun = query.only('id', 'owner_id')    # partial fields
-    pat2b = await torun
-    await pat2b.fetch_related('owner')  # +1q, all fields
-    the_owner = pat2b.owner
-    ic(type(pat2b), vars(pat2b), type(the_owner), vars(the_owner))
+    # torun = query.only('id', 'owner_id')    # partial fields
+    # pat2b = await torun
+    # await pat2b.fetch_related('owner')  # +1q, all fields
+    # the_owner = pat2b.owner
+    # ic(type(pat2b), vars(pat2b), type(the_owner), vars(the_owner))
     
     """
     PATTERN 2c: ForeignKeyField
@@ -91,12 +85,12 @@ async def dev_user_data(_: Response, user=Depends(current_user)):
     TIP: If you're only reading, use values() or values_list() to produce a more efficient query
     TOTAL QUERIES: 2
     """
-    query = query.only('id', 'owner_id')        # 1q, partial fields
-    torun = query.prefetch_related('owner')     # +1q, all fields
-    # torun = query.only('id', 'owner_id').prefetch_related('owner__anotherfield')  # 3q, all fields
-    pat2c = await torun
-    the_owner = pat2c.owner
-    ic(type(pat2c), vars(pat2c), type(the_owner), vars(the_owner))
+    # query = query.only('id', 'owner_id')        # 1q, partial fields
+    # torun = query.prefetch_related('owner')     # +1q, all fields
+    # # torun = query.only('id', 'owner_id').prefetch_related('owner__anotherfield')  # 3q, all fields
+    # pat2c = await torun
+    # the_owner = pat2c.owner
+    # ic(type(pat2c), vars(pat2c), type(the_owner), vars(the_owner))
     
     """
     PATTERN 2d (Recommended): ForeignKeyField
@@ -104,156 +98,161 @@ async def dev_user_data(_: Response, user=Depends(current_user)):
     - The owner attr contains an object w/ partial fields
     - Variation of PATTERN 2c using prefetch_related + Prefetch
     - Allows for partial fields. The owner_id field must exist.
+    - Using only() in Prefetch only works because it's a FK not M2M
     TIP: If you're only reading, use values() or values_list() to produce a more efficient query
     TOTAL QUERIES: 2
     """
-    query = query.only('id', 'owner_id')    # 1q, partial fields
-    torun = query.prefetch_related(
-        Prefetch('owner', Owner.all().only('id', 'name')),       # +1q, partial fields
-        # Prefetch('field1', SomeModel.all()),      # +1q, all fields
-        # Prefetch('field2', SomeTable.filter(foo=True).all()), # +1q, all fields, list
-        # 'field3'   # +1q, all fields, might be list
-    )
-    pat2d = await torun
-    the_owner = pat2d.owner
-    ic(type(pat2d), vars(pat2d), type(the_owner), vars(the_owner))
-    
-    # # 2.3b: Variation of 2.3a with more FK fields
-    # b3b = await query.only('id', 'owner_id', 'author_id', 'exchange_id').prefetch_related(
-    #     Prefetch('owner', queryset=Owner.filter(deleted_at=None).only('id', 'name')),
-    #     Prefetch('author', queryset=UserMod.filter(deleted_at=None).only('id', 'display')),
-    #     'exchange',                                                         # All fields
+    # query = query.only('id', 'owner_id')    # 1q, partial fields
+    # torun = query.prefetch_related(
+    #     Prefetch('owner', Owner.all().only('id', 'name')),       # +1q, partial fields
+    #     # Prefetch('field1', SomeModel.all()),      # +1q, all fields
+    #     # Prefetch('field2', SomeTable.filter(foo=True).all()), # +1q, all fields, list
+    #     # 'field3'   # +1q, all fields, might be list
     # )
-    # the_sql1 = query.only('id', 'owner_id', 'author_id', 'exchange_id').prefetch_related(
-    #     Prefetch('owner', queryset=Owner.filter(deleted_at=None).only('id', 'name')),
-    #     Prefetch('author', queryset=UserMod.filter(deleted_at=None).only('id', 'display')),
-    #     'exchange',
-    # ).sql()
-    # the_owner = b3b.owner           # Select fields
-    # the_author = b3b.author         # Select fields
-    # the_exchange = b3b.exchange     # All fields
-    # ic(type(b3b), vars(b3b), vars(the_owner), vars(the_author), vars(the_exchange), the_sql1)
+    # pat2d = await torun
+    # the_owner = pat2d.owner
+    # ic(type(pat2d), vars(pat2d), type(the_owner), vars(the_owner))
+    
+    """
+    -------------------------------------------------------------------------------------------
+    VERDICT FK: For FKs use prefetch_related + Prefetch for partial fields. BUT if you only want
+    to get the data and not manipulate the object or call methods then use PATTERN 1 instead
+    NOTE:
+    - prefetch_related: Used on the QuerySet (recommended)
+    - fetch_related: Used on the QuerySet instance
+    - Both work for FKs and M2Ms and do the same thing
+    - For both make sure you called the field_id in the main query or it won't work
+    - Nested Prefetch will work
+    -------------------------------------------------------------------------------------------
+    """
+    
 
-    # # 2.3c: Variation of 2.3a. Uses fetch_related to add a field (maybe you thought you didn't
-    # # need it). The field_id of the related table must already be there to work.
-    # x = query.only('id', 'owner_id', 'author_id').prefetch_related(
-    #     Prefetch('owner', queryset=Owner.filter(deleted_at=None).only('id', 'name'), to_attr='xxx'),
-    #     # Prefetch('author', queryset=UserMod.filter(deleted_at=None).only('id', 'display')),
-    #     # 'exchange',  # All fields
-    # )
-    # b3c = await x
-    # the_sql1 = x.sql()
-    # the_owner = b3c.owner  # Select fields
-    # ic(vars(b3c))
-    # await b3c.fetch_related('author')       # Addt'l query
-    # the_author = b3c.author  # Select fields
-    # ic(vars(b3c), vars(the_owner), vars(the_author), the_sql1)
-    
-    
-    # VERDICT: For FKs use prefetch_related + Prefetch for partial fields. BUT if you only want
-    # to get the data and not manipulate the object or call methods then use PATTERN 1 instead
-    # NOTE:
-    #   - prefetch_related: Used on the QuerySet (recommended)
-    #   - fetch_related: Used on the QuerySet instance
-    #   - Both work for FKs and M2Ms and do the same thing
-    #   - For both make sure you called the field_id in the main query or it won't work.
-    
-    # -------------------------------------------------------------------------------------------
-
-    # M2M Table
-    # Get for only one entry
+    # M2M Relationships
     query = UserMod.get(email=VERIFIED_EMAIL_DEMO)
-    query_fiter = UserMod.filter(deleted_at=None)
-    
-    # # Wrong way: You need to bake a 2nd query to get the m2m
-    # # Also prefetch_related is irrelevant here
-    # c1 = await query.only('id', 'display').prefetch_related('brokers')      # 1q
-    # broker_list = await c1.brokers                                          # +1q
-    # ic(type(c1), vars(c1), broker_list)
-    
-    # # Right way: Using Prefetch with to_attr
-    # # For a single item
-    # x = query.only('id', 'display').prefetch_related(
-    #     Prefetch('brokers', Broker.all(), to_attr='broker_list'),       # only() does nothing
-    #     Prefetch('permissions', Permission.all(), to_attr='perm_list'), # only() does nothing
-    # )
-    # c2 = await x
-    # the_sql = x.sql()
-    # ic(type(c2), vars(c2), vars(c2.broker_list[0]), the_sql)
 
-    # # Nested Prefetch doesn't work. You'll have to run it manuall after the first Prefetch.
-    # x = query.only('id', 'display').prefetch_related(
+    """
+    PATTERN 3a: ManyToManyField
+    USES:
+    - The brokers.related_objects attr contains a list of objects w/ all fields
+    - only() will not work with broker_list as it's now M2M not FK. broker_id isn't required since
+      there is no physical M2M field in both tables.
+    TOTAL QUERIES: 2+?
+    """
+    # torun = query.only('id', 'display')
+    # pat3a = await torun
+    # await pat3a.fetch_related('brokers')
+    # broker_list = pat3a.brokers.related_objects
+    # ic(type(pat3a), vars(pat3a), type(broker_list[0]), vars(broker_list[0]))
+    
+    """
+    PATTERN 3b: ManyToManyField (Recommended)
+    USES:
+    - The broker_list attr contains a list of objects w/ all fields
+    - Variation of PATTERN 3a using prefetch_related + Prefetch
+    - only() will not work with broker_list as it's now M2M not FK. broker_id isn't required since
+      there is no physical M2M field in both tables.
+    TIP: If you want partial fields for broker_list you'll have to go through the traversing of
+    tables including the intermediary table manually. For this query that means going through the
+    UserMod->UserBrokers->Brokers tables (3q total at least).
+    TOTAL QUERIES: 2+?
+    """
+    # torun = query.only('id', 'display').prefetch_related(
+    #     Prefetch('brokers', Broker.all(), to_attr='broker_list'),       # +1q, all fields
+    # )
+    # pat3b = await torun
+    # broker_list = pat3b.broker_list
+    # ic(type(pat3b), vars(pat3b), type(broker_list[0]), vars(broker_list[0]))
+    
+    """
+    PATTERN 3c: ManyToManyField
+    USES:
+    - The broker_list attr contains a list of objects w/ all fields
+    - Variation of PATTERN 3b but trying to get the author from broker as well
+    - To get author of a broker you'll need to use fetch_related on each broker
+    NOTE: There must be a better way
+    TOTAL QUERIES: 3+?
+    """
+    # # WRONG WAY
+    # torun = query.only('id', 'display').prefetch_related(
+    #     # Fail 1: Nested Prefetch -> doesn't work
     #     Prefetch('brokers', Broker.all().prefetch_related(
-    #         Prefetch('author', UserMod.all(), to_attr='xxx')    # Does nothing
-    #     ), to_attr='broker_list'),
+    #         Prefetch('author', UserMod.all(), to_attr='xxx')    # does nothing
+    #     ), to_attr='broker_list'),       # +1q, all fields
+    #
+    #     # Fail 2: Separate Prefetch -> doesn't work
+    #     # Prefetch('brokers', Broker.all(), to_attr='broker_list'),
+    #     # Prefetch('brokers__author', UserMod.all(), to_attr='xxx')  # does nothing
     # )
-    # c3a = await x
-    # z = await c3a.broker_list[0].author.only('id', 'display')
-    # the_sql = x.sql()
-    # ic(type(c3a), vars(c3a), vars(c3a.broker_list[0]), type(c3a.broker_list[0].author), vars(z),
-    #    the_sql)
-
-    # # Alternative to c3a
-    # x = query.only('id', 'display').prefetch_related(
-    #     Prefetch('brokers', Broker.all(), to_attr='broker_list'),
+    
+    # # RIGHT WAY
+    # torun = query.only('id', 'display').prefetch_related(
+    #     Prefetch('brokers', Broker.all(), to_attr='broker_list')    # same as 3b
     # )
-    # c3b = await x
-    # await c3b.broker_list[0].fetch_related('author')
-    # broker1 = c3b.broker_list[0].author     # UserMod, bec you used fetch_related, +1q
-    # broker2 = c3b.broker_list[1].author     # QuerySet
-    # broker3 = c3b.broker_list[2].author     # QuerySet
-    # the_sql = x.sql()
-    # ic(type(c3b), vars(c3b), vars(c3b.broker_list[0]),
-    #    type(broker1), type(broker2), type(broker3),
-    #    vars(broker1),
-    #    the_sql)
+    # pat3c = await torun
+    # broker_list = pat3c.broker_list
+    # await broker_list[0].fetch_related('author')    # +1q, all fields, single broker
+    # ic(
+    #     type(pat3c), vars(pat3c),
+    #     type(broker_list[0]), vars(broker_list[0]),     # author is UserMod (bec of fetch_related)
+    #     type(broker_list[1]), vars(broker_list[1]),     # no author
+    # )
+    
+    """
+    -------------------------------------------------------------------------------------------
+    VERDICT M2M: For M2Ms use prefetch_related + Prefetch and only() will not work since it's now
+    an M2M field not a FK field.
+    NOTE:
+    - field_id is not needed anymore since in M2M there is no physical field attached to the table
+    - To get a sub-relation (e.g. author_id) you'll need to use fetch_related on each one manually
+    -------------------------------------------------------------------------------------------
+    """
 
-    # For multiple items + Relationship table + Nested Prefetch
-    # Same thing except a list is returned instead of a single object
-    # Remember, only() works if you're NOT using the M2M field instead traversing the tables
-    # manuolly
-    x = query_fiter.only('id', 'display').prefetch_related(
-        # # only() no use here (brokers is M2M)
-        Prefetch('brokers', Broker.all(), to_attr='broker_list'),
-        # # only() works here as noted above
-        # Prefetch('userbrokers', UserBrokers.all().only('id', 'is_primary', 'user_id', 'broker_id')
-        #      .prefetch_related(
-        #         Prefetch('broker', Broker.all().only('id', 'name', 'author_id').prefetch_related(
-        #             Prefetch('author', UserMod.all().only('id', 'display'), to_attr='zzz')
-        #         ), to_attr='yyy')
-        #     ), to_attr='xxx'),
-    )
-    c3 = await x
-    the_sql1 = x.sql()
-    # broker1 = c3[0].broker_list[0]
-    # userbrokers1 = c3[0].xxx[0]
-    # boo1 = userbrokers1.yyy
-    # auth1 = boo1.author
-    # broker2 = c3[0].broker_list[1]
-    # userbrokers2 = c3[0].xxx[1]
-    # boo2 = userbrokers2.yyy
-    # auth2 = boo2.author
-    # ic(type(c3), c3, vars(c3[0]), type(c3[0]),
-    #    type(broker1), vars(broker1), type(userbrokers1), vars(userbrokers1),
-    #    type(boo1), vars(boo1), type(auth1), vars(auth1),
+    # Traversing Relationships Manually
+    query = UserMod.filter(deleted_at=None)
+    
+    """
+    PATTERN 4: Traversing tables manually (FK and M2M)
+    USES:
+    - Use only if you need data from the rel'p table
+    - only() not work since you're basically making FK calls
+    TOTAL QUERIES: 4+?
+    """
+    # torun = query.only('id', 'display').prefetch_related(
+    #     # user_id, broker_id needed for userbrokers, broker, respectively since it's a rel'p table
+    #     Prefetch('userbrokers', UserBrokers.all().only('id', 'is_primary', 'user_id', 'broker_id')
+    #      .prefetch_related(
+    #         # author_id needed for author
+    #         Prefetch('broker', Broker.all().only('id', 'name', 'author_id').prefetch_related(
+    #             Prefetch('author', UserMod.all().only('id', 'display'))   # +1q, partial fields
+    #         ))  # +1q, partial fields
+    #     ), to_attr='xuserbrokers_list'),    # +1q, partial fields
+    # )
+    # pat4 = await torun
+    # user1 = pat4[0]
+    # xuserbrokers1 = user1.xuserbrokers_list[0]
+    # broker1 = xuserbrokers1.broker
+    # auth1 = broker1.author
+    # xuserbrokers2 = user1.xuserbrokers_list[1]
+    # broker2 = xuserbrokers2.broker
+    # auth2 = broker2.author
+    # ic(type(pat4), pat4,
+    #    type(user1), vars(user1),
+    #    type(xuserbrokers1), vars(xuserbrokers1),
+    #    type(broker1), vars(broker1),
+    #    type(auth1), vars(auth1),
+    #    type(xuserbrokers2), vars(xuserbrokers2),
     #    type(broker2), vars(broker2),
-    #    type(userbrokers2), vars(userbrokers2),
-    #    type(boo2), vars(boo2), type(auth2), vars(auth2),
-    #    the_sql1,
+    #    type(auth2), vars(auth2),
     # )
-
-    for i in c3:
-        ic(f'{type(i)}: {i.display}')
-        for j in i.brokers:
-            ic(j)
-    ic(the_sql1)
     
-    
-    # VERDICT:
-    #   - Using the M2M fields do not allow the use of only() for Prefetch
-    #   - Alternatively, traverse the tables intead of using the M2M fields and only() works
-    #   - Nested Prefetch works when traversing the tables manually. Won't work if you're using
-    #     the M2M fields
+    """
+    -------------------------------------------------------------------------------------------
+    VERDICT Manual: Use manual traversion of rel'p tables if you need data from the rel'p table
+    NOTE:
+    - Similar to FKs except the rel'p table has 2 kinds of field_id not 1
+    - only() works here since you're basically traversing tables using FKs
+    -------------------------------------------------------------------------------------------
+    """
     
     # log.info('info here')
     # log.warning('warning here')
