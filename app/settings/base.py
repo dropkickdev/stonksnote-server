331cc175c409080fe8685a2580bdb509613a7ed5
enchance <enchance@gmail.com>
2021-08-01 15:09:04 +0800

    Update base settings

--- app/settings/base.py
+++ app/settings/base.py
@@ -24,7 +24,7 @@ class Base(BaseSettings):
     ALGORITHM: str = 'HS256'
     ACCESS_TOKEN_EXPIRE: int = 60 * 15  # seconds (15 mins)
     REFRESH_TOKEN_EXPIRE: int = 3600 * 24 * 15  # seconds (15 days)
-    REFRESH_TOKEN_CUTOFF: int = 15  # minutes
+    REFRESH_TOKEN_CUTOFF: int = ACCESS_TOKEN_EXPIRE  # minutes
     SESSION_COOKIE_AGE: int = 3600 * 24 * 15  # seconds
     VERIFY_EMAIL_TTL: int = 3600 * 3  # seconds
     RESET_PASSWORD_TTL: int = 60 * 30  # seconds
